04a371d92a11e96d81b11794165718972a150075
Blesilda Ramirez <ramirezblesilda@gmail.com>
2024-04-30 19:49:11 +0800

    pkp/pkp-lib#9890 Revert changes related to outdated files

--- src/App.vue
+++ src/App.vue
@@ -84,9 +84,6 @@
 					MultilingualProgress
 				</router-link>
 			</li>
-			<li>
-				<router-link to="/component/Navigation">Navigation</router-link>
-			</li>
 			<li><router-link to="/component/Orderer">Orderer</router-link></li>
 			<li>
 				<router-link to="/component/Pagination">Pagination</router-link>
