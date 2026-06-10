045c5cdf0d4d1f56f5bf72e3fe02b66a7076f59a
Blesilda Biazon <ramirezblesilda@gmail.com>
2026-06-10 13:42:25 +0800

    Add Date, User and Download link for each logs in tasks and discussions

--- src/managers/DiscussionManager/DiscussionManagerHistoryModal.vue
+++ src/managers/DiscussionManager/DiscussionManagerHistoryModal.vue
@@ -9,14 +9,33 @@
 		<SideModalLayoutBasic>
 			<PkpTable>
 				<TableHeader>
+					<TableColumn>{{ t('common.date') }}</TableColumn>
+					<TableColumn>{{ t('common.user') }}</TableColumn>
 					<TableColumn>{{ t('common.event') }}</TableColumn>
+					<TableColumn>
+						<span class="-screenReader">{{ t('common.download') }}</span>
+					</TableColumn>
 				</TableHeader>
 				<TableBody>
 					<TableRow
 						v-for="activity in workItem.latestActivities"
 						:key="activity.id"
 					>
+						<TableCell>{{ formatShortDate(activity.date) }}</TableCell>
+						<TableCell>{{ activity.userFullName }}</TableCell>
 						<TableCell>{{ activity.message }}</TableCell>
+						<TableCell>
+							<div class="flex justify-end">
+								<PkpButton
+									v-if="activity.downloadUrl"
+									element="a"
+									is-link
+									:href="activity.downloadUrl"
+								>
+									{{ t('common.download') }}
+								</PkpButton>
+							</div>
+						</TableCell>
 					</TableRow>
 				</TableBody>
 			</PkpTable>
@@ -25,6 +44,7 @@
 </template>
 <script setup>
 import {t} from '@/utils/i18n';
+import {useDate} from '@/composables/useDate';
 
 import SideModalBody from '@/components/Modal/SideModalBody.vue';
 import SideModalLayoutBasic from '@/components/Modal/SideModalLayoutBasic.vue';
@@ -34,6 +54,9 @@ import TableColumn from '@/components/Table/TableColumn.vue';
 import TableBody from '@/components/Table/TableBody.vue';
 import TableRow from '@/components/Table/TableRow.vue';
 import TableCell from '@/components/Table/TableCell.vue';
+import PkpButton from '@/components/Button/Button.vue';
+
+const {formatShortDate} = useDate();
 
 defineProps({workItem: {type: Object, required: true}});
 </script>
